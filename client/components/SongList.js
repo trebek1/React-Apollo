c1276ae929936e2040c3821ab196ed96455a51ae
Alex Trzeciak <trebek08@gmail.com>
2018-04-26 21:22:22 -0500

    enhanced routing with react app

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
 	renderSongs(){
@@ -15,9 +16,15 @@ class SongList extends Component {
 		if(loading){
 			return <div> Loading... </div>;	
 		}
-		return <ul className="collection">
+		return <div> <ul className="collection">
 			{ this.renderSongs() }
-		</ul>
+		</ul> 
+		<Link to="/songs/new"
+		className="btn-floating btn-large red right"
+		>
+		<i className="material-icons">add</i>
+		</Link>
+		</div>
 	}
 }
 
